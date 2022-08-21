4c4f6482923d1c1b950a6835f2485177e2a34f46
user3107 <0alexchebanu@gmail.com>
2022-08-21 18:33:36 +0300

    initial

---
+++ Library.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace LibraryOOP
+{
+    public class Library
+    {
+        private int libraryID;
+        private string userName;
+        private string bookName;
+
+        public Library(int libraryID, string userName, string bookName)
+        {
+            this.libraryID = libraryID;
+            this.userName = userName;
+            this.bookName = bookName;
+        }
+
+        public void print()
+        {
+            Console.WriteLine($"ID {libraryID}\nName: {userName}\tBook: {bookName}");
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace LibraryOOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var first = new Library( 121,"Alex", "M Twain");
+            var second = new Library(122, "Anthony", "Formula");
+            var third = new Library(32242, "Liza", "Time");
+            
+            first.print();
+            second.print();
+            third.print();
+        }
+    }    
+}
